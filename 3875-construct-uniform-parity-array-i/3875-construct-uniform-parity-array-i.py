02b5787a268e9a9e15a6d96a2d834a33a3c1c9b7
Md Rehan Alam <rehanalam201006@gmail.com>
2026-09-02 21:43:03 +0530

    Time: 0 ms (100%), Space: 12.4 MB (65%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        # It is mathematically guaranteed to always be possible.
+        return True
